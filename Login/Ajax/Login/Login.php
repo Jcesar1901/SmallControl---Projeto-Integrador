cd88004fb85469e040e1b8edc7a74183fd7257fe
Jcesar1901 <jcesarc19@hotmail.com>
2023-11-27 17:40:02 -0400

    Commit_26_11

--- Login/Ajax/Login/Login.php
+++ Login/Ajax/Login/Login.php
@@ -1,3 +1,94 @@
 <?php
     require '../../Developers/Config.php';
+    $Post = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRIPPED);
+    $PostFilters = array_map('strip_tags', $Post);
+    
+    $message = null;
+    
+    $counter = 0;
+    
+    //Responsavel por calulcar o numero de tentativas de login
+    if($counter == TIMESBLOCKED){
+        $_SESSION['counter'] = $counter + 1;
+        $message = ['status' => 'warning', 'message'=>'Você só possui mais uma tentativa', 'redirect'=>''];
+        echo json_encode($message);
+        return;
+    }
+
+    //Bloqueio de acesso
+    if(BLOCKED == 1 %% $counter == TIMESBLOCKED || $_SESSION['blocked'] == 1) {
+        unset($_SESSION['user_name']);
+        unset($_SESSION['user_level']);
+        unset($_SESSION['user_email']);
+        unset($_SESSION['user_id']);
+        unset($_SESSION['user_token']);
+        unset($_SESSION['logged']);
+
+        $_SESSION['blocked'] = 1;
+        setcookie("LBlocked", 1, 86400, '/');
+        $message = ['status' => 'error', 'message'=>'Seu acesso foi blockeado! fale com o admin!', 'redirect'=>''];
+        echo json_encode($message);
+        return;
+    }
+    //Verifica se o email é valido
+    $Email = $PostFilters['login_email'];
+    if(!$Email || empty($Email) || $Email == null || filter_var($Email, FILTER_VALIDATE_EMAIL)){
+
+        $message = ['status' => 'info', 'message'=>'Email invalido !', 'redirect'=>''];
+        echo json_encode($message);
+        return;
+    }
+
+    //Consulta para verificar se o email já existe
+    $Read = $pdo->prepare("SELECT user_id, user_email, user_password, user_firstname, user_lastname, user_token FROM users WHERE user_email = :user_email");
+    $Read->bindValue(':user_email', $Email);
+    $Read->execute();
+
+    $Lines = $Read->rowCount();
+
+    if($Lines == 0 ){
+        $_SESSION['counter'] = $counter + 1;
+        
+        if($counter == TIMESBLOCKED){
+            $message = ['status' => 'warning', 'message'=>'Você só possui mais uma tentativa', 'redirect'=>''];
+            echo json_encode($message);
+            return;
+        }else{
+            $message = ['status' => 'info', 'message'=>'Email ou senha incorretos', 'redirect'=>''];
+            echo json_encode($message);
+            return;
+        }
+    }
+
+    //Recuperando os dados
+    foreach($Read as $Show){}
+
+    //Verificar e checar a senha
+    $VerifyPass = password_verify($PostFilters['login_password'], $Show['user_password']);
+
+    //Verifica se o model de lembrar senha está ativo
+    if($VerifyPass){
+        if(!empty($PostFilters['login_remember'])){
+            //Cookie valido por 30 dias
+            $time = 3600*24*30;
+            $Email = $PostFilters['login_email'];
+            $pass = $PostFilters['login_password'];
+            
+            setcookie("LE", $Email, $time + 3600, '/');
+            setcookie("LP", $pass, $time + 3600, '/');
+        }
+        // Cria as sessões de acesso
+        $_SESSION['user_id'] = $Show['user_id'];
+        $_SESSION['user_name'] = $Show['user_firstname']. ' ' . $Show['user_lastname'];
+        $_SESSION['user_email'] = $Show['user_email'];
+        $_SESSION['user_level'] = $Show['user_level'];
+        $_SESSION['user_token'] = $Show['user_token'];
+        $_SESSION['logged'] = 1;
+
+        unset($_SESSION['counter']);
+        
+        $message = ['status' => 'success', 'message'=>'Login realizado com sucesso!', 'redirect'=>'Admin/home.php'];
+        echo json_encode($message);
+        return;
+    }
 ?>
--- Login/Ajax/Login/Logout.php
+++ Login/Ajax/Login/Logout.php
@@ -1,3 +1,26 @@
 <?php
     require '../../Developers/Config.php';
+    $Get = filter_input_array(INPUT_GET, 'action', FILTER_SANITIZE_STRIPPED);
+    $GetFilters = array_map('strip_tags', $Get);
+    
+    $message = null;
+
+    if(!$Get || $Get = '' || $Get = null){
+        $message = ['status' => 'warning', 'message'=>'Nenhuma ação pode ser realizada!', 'redirect'=>'home.php'];
+        echo json_encode($message);
+        return;
+    }else{
+        session_destroy();
+        unset($_SESSION['user_name']);
+        unset($_SESSION['user_level']);
+        unset($_SESSION['user_email']);
+        unset($_SESSION['user_id']);
+        unset($_SESSION['user_token']);
+        unset($_SESSION['logged']);
+    
+
+        $message = ['status' => 'success', 'message'=>'Logout realizado com sucesso!', 'redirect'=>'../login.php'];
+        echo json_encode($message);
+        return;
+    }
 ?>
