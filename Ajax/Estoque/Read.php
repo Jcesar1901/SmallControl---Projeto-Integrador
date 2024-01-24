0b6fd488a6e51b9746077aad5220f699e170e4a8
Jcesar1901 <jcesarc19@hotmail.com>
2024-01-24 17:25:42 -0400

    Commit_24_01_24

--- Ajax/Estoque/Read.php
+++ Ajax/Estoque/Read.php
@@ -25,6 +25,12 @@ if($Search['type'] == 1 && empty($Search['searching'])){
 
     $Lines = $Read->rowCount();
 
+    if($Lines == 0){
+        $message = ["status" => "info", "message" => "Não foi encontrado nenhum resultado!", "Lines" => 0];
+        echo json_encode($message);
+        return; 
+    }
+
     foreach($Read as $Show){
         $pedido[] = strip_tags($Show['entrada_codigo']);
         $nf[] = strip_tags($Show['entrada_nf']);
@@ -47,6 +53,12 @@ if($Search['type'] == 2 && empty($Search['searching'])){
 
     $Lines = $Read->rowCount();
 
+    if($Lines == 0){
+        $message = ["status" => "info", "message" => "Não foi encontrado nenhum resultado!", "Lines" => 0];
+        echo json_encode($message);
+        return; 
+    }
+
     foreach($Read as $Show){
         $pedido[] = strip_tags($Show['saida_codigo']);
         $nf[] = strip_tags($Show['saida_nf']);
@@ -68,6 +80,12 @@ if($Search['type'] == 3 && empty($Search['searching'])){
 
     $Lines = $Read->rowCount();
 
+    if($Lines == 0){
+        $message = ["status" => "info", "message" => "Não foi encontrado nenhum resultado!", "Lines" => 0];
+        echo json_encode($message);
+        return; 
+    }
+
     foreach($Read as $Show){
         $pedido[] = strip_tags($Show['devolucao_codigo']);
         $nf[] = strip_tags($Show['devolucao_nf']);
@@ -83,30 +101,171 @@ if($Search['type'] == 3 && empty($Search['searching'])){
 
 // CANCELADO 
 if($Search['type'] == 3 && empty($Search['searching'])){
-    $Read = $pdo->prepare("SELECT devolucao_sessao FROM " . DB_DEVOLUTION . " WHERE devolucao_sessao = :devolucao_sessao");
-    $Read->bindValue(':devolucao_sessao', $Session);
+    $Read = $pdo->prepare("SELECT a.entrada_nf, a.entrada_codigo, a.entrada_produto_id, a.entrada_status, b.saida_nf, b.saida_codigo, b.saida_id, b.saida_status, c.devolucao_nf, c.devolucao_codigo, c.devolucao_id, c.devolucao_status FROM si_entrada a INNER JOIN si_saida b ON (b.saida_nf = a.entrada_nf) INNER JOIN si_devolucao c ON (c.devolucao_nf = a.entrada_nf) WHERE a.entrada_status = :entrada_status");
+    $Read->bindValue(':entrada_status', 0);
     $Read->execute();
+
+    $Lines = $Read->rowCount();
+
+    if($Lines == 0){
+        $message = ["status" => "info", "message" => "Não foi encontrado nenhum resultado!", "Lines" => 0];
+        echo json_encode($message);
+        return; 
+    }
+    
+    $pedido = [];
+    $nf = [];
+    $status = [];
+    $id = [];
+    $operacao = [];
+    
+    foreach($Read as $Show){
+        $pedido[] = strip_tags($Show['entrada_codigo']);
+        $nf[] = strip_tags($Show['entrada_nf']);
+        $status[] = strip_tags($Show['entrada_status'] == 1 ? 'Ativo' : 'Inativo');
+        $id[] = strip_tags($Show['entrada_produto_id']);
+        $operacao[] = 'Entrada';
+
+        $pedido[] = strip_tags($Show['saida_codigo']);
+        $nf[] = strip_tags($Show['saida_nf']);
+        $status[] = strip_tags($Show['saida_status'] == 1 ? 'Ativo' : 'Inativo');
+        $id[] = strip_tags($Show['saida_id']);
+        $operacao[] = 'Saída';
+
+        $pedido[] = strip_tags($Show['devolucao_codigo']);
+        $nf[] = strip_tags($Show['devolucao_nf']);
+        $status[] = strip_tags($Show['devolucao_status'] == 1 ? 'Ativo' : 'Inativo');
+        $id[] = strip_tags($Show['devolucao_id']);
+        $operacao[] = 'Devolução';
+
+        $message = ["pedido" => $pedido, "nf" => $nf, "stat" => $status, "id" => $id, "operacao" => $operacao];
+    }
+    
+    
+    echo json_encode($message);
+    return;
 }
 
 // PEDIDO
 if($Search['type'] == 'n' && !empty($Search['searching'])){
-    $Read = $pdo->prepare("SELECT entrada_nf, entrada_codigo, entrada_produto_id, entrada_status FROM " . DB_STOCKIN . " WHERE entrada_nf = :entrada_nf");
+    $Read = $pdo->prepare("SELECT a.entrada_nf, a.entrada_codigo, a.entrada_produto_id, a.entrada_status, b.saida_nf, b.saida_codigo, b.saida_id, b.saida_status, c.devolucao_nf, c.devolucao_codigo, c.devolucao_id, c.devolucao_status 
+    FROM si_entrada a 
+    INNER JOIN si_saida b ON (b.saida_nf = a.entrada_nf OR b.saida_nf = '')
+    INNER JOIN si_devolucao c ON (c.devolucao_nf = a.entrada_nf OR c.devolucao_nf = '')
+    WHERE a.entrada_nf = :entrada_nf");
     $Read->bindValue(':entrada_nf', $Search['searching']);
     $Read->execute();
+
     $Lines = $Read->rowCount();
 
-    foreach($Read as $Show){}
+    if($Lines == 0){
+        $message = ["status" => "info", "message" => "Não foi encontrado nenhum resultado!", "Lines" => 0];
+        echo json_encode($message);
+        return; 
+    }
+    
+    foreach($Read as $Show){
+        $pedido[] = strip_tags($Show['entrada_codigo']);
+        $nf[] = strip_tags($Show['entrada_nf']);
+        $status[] = strip_tags($Show['entrada_status'] == 1 ? 'Ativo' : 'Inativo');
+        $id[] = strip_tags($Show['entrada_produto_id']);
+        $operacao[] = 'Entrada';
 
-    $pedido = strip_tags($Show['entrada_codigo']);
-    $nf = strip_tags($Show['entrada_nf']);
-    $status = strip_tags($Show['entrada_status'] == 1 ? 'Ativo' : 'Inativo');
-    $id = strip_tags($Show['entrada_produto_id']);
-    $operacao = 'Entrada';
+        $pedido[] = strip_tags($Show['saida_codigo']);
+        $nf[] = strip_tags($Show['saida_nf']);
+        $status[] = strip_tags($Show['saida_status'] == 1 ? 'Ativo' : 'Inativo');
+        $id[] = strip_tags($Show['saida_id']);
+        $operacao[] = 'Saída';
+
+        $pedido[] = strip_tags($Show['devolucao_codigo']);
+        $nf[] = strip_tags($Show['devolucao_nf']);
+        $status[] = strip_tags($Show['devolucao_status'] == 1 ? 'Ativo' : 'Inativo');
+        $id[] = strip_tags($Show['devolucao_id']);
+        $operacao[] = 'Devolução';
+
+        $message = ["pedido" => $pedido, "nf" => $nf, "stat" => $status, "id" => $id, "operacao" => $operacao];
+    }
+    
+    
+    echo json_encode($message);
+    return; 
 }
 
 // PEDIDO + TIPO DE OPERAÇÃO
-if($Search['type'] =! 'n' && empty($Search['searching'])){
-    $Read = $pdo->prepare("SELECT entrada_sessao FROM " . DB_STOCKIN . " WHERE entrada_sessao = :entrada_sessao");
-    $Read->bindValue(':entrada_sessao', $Session);
-    $Read->execute();
+if($Search['type'] != 'n' && !empty($Search['searching'])){
+    if($Search['type'] == 1){ //ENTRADA
+        $Read = $pdo->prepare("SELECT entrada_nf, entrada_codigo, entrada_produto_id, entrada_status
+        FROM si_entrada WHERE entrada_nf = :entrada_nf");
+        $Read->bindValue(':entrada_nf', $Search['searching']);
+        $Read->execute();
+
+        $Lines = $Read->rowCount();
+
+        if($Lines == 0){
+            $message = ["status" => "info", "message" => "Não foi encontrado nenhum resultado!", "Lines" => 0];
+            echo json_encode($message);
+            return; 
+        }
+        
+        foreach($Read as $Show){
+            $pedido[] = strip_tags($Show['entrada_codigo']);
+            $nf[] = strip_tags($Show['entrada_nf']);
+            $status[] = strip_tags($Show['entrada_status'] == 1 ? 'Ativo' : 'Inativo');
+            $id[] = strip_tags($Show['entrada_produto_id']);
+            $operacao[] = 'Entrada';
+
+            $message = ["pedido" => $pedido, "nf" => $nf, "stat" => $status, "id" => $id, "operacao" => $operacao];
+        }
+    }else if($Search['type'] == 2){ // SAÍDA
+        $Read = $pdo->prepare("SELECT saida_nf, saida_codigo, saida_id, saida_status
+        FROM si_saida WHERE saida_nf = :saida_nf");
+        $Read->bindValue(':saida_nf', $Search['searching']);
+        $Read->execute();
+
+        $Lines = $Read->rowCount();
+
+        if($Lines == 0){
+            $message = ["status" => "info", "message" => "Não foi encontrado nenhum resultado!", "Lines" => 0];
+            echo json_encode($message);
+            return; 
+        }
+        
+        foreach($Read as $Show){
+            $pedido[] = strip_tags($Show['saida_codigo']);
+            $nf[] = strip_tags($Show['saida_nf']);
+            $status[] = strip_tags($Show['saida_status'] == 1 ? 'Ativo' : 'Inativo');
+            $id[] = strip_tags($Show['saida_id']);
+            $operacao[] = 'Saída';
+
+            $message = ["pedido" => $pedido, "nf" => $nf, "stat" => $status, "id" => $id, "operacao" => $operacao];
+        }
+    }else if($Search['type'] == 3){ // DEVOLUÇÃO
+            $Read = $pdo->prepare("SELECT devolucao_nf, devolucao_codigo, devolucao_id, devolucao_status
+            FROM si_devolucao WHERE devolucao_nf = :devolucao_nf");
+            $Read->bindValue(':devolucao_nf', $Search['searching']);
+            $Read->execute();
+    
+            $Lines = $Read->rowCount();
+
+            if($Lines == 0){
+                $message = ["status" => "info", "message" => "Não foi encontrado nenhum resultado!", "Lines" => 0];
+                echo json_encode($message);
+                return; 
+            }
+            
+            foreach($Read as $Show){
+                $pedido[] = strip_tags($Show['devolucao_codigo']);
+                $nf[] = strip_tags($Show['devolucao_nf']);
+                $status[] = strip_tags($Show['devolucao_status'] == 1 ? 'Ativo' : 'Inativo');
+                $id[] = strip_tags($Show['devolucao_id']);
+                $operacao[] = 'Devolução';
+    
+                $message = ["pedido" => $pedido, "nf" => $nf, "stat" => $status, "id" => $id, "operacao" => $operacao];
+            }
+    }else if($Search['type'] == 4){ // CANCELADO   
+        $message = ["status" => "info", "message" => "Não é possivel buscar NF no estado cancelado", "Lines" => 0];
+    }
+    
+        echo json_encode($message);
+        return; 
 }
